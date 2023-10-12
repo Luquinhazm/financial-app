79c3907b1350a9f1a9fb9119c52a328448de53f8
luquinhazm <lucsmends09@gmail.com>
2023-10-12 12:38:11 -0300

    organização estilos

--- src/components/Cards/CardExpenses/CardExpenses.jsx
+++ src/components/Cards/CardExpenses/CardExpenses.jsx
@@ -4,15 +4,15 @@ import chooseTextColor from "@/utils/chooseTextColor"
 export default function CardExpenses({ grupo }) {
     const totalDespesas = grupo.despesas.reduce((total, despesa) => total + despesa.valor, 0);
     return (
-        <div>
+        <div className="p-2 shadow w-full ">
             <div className='flex items-center justify-between gap-1'>
                 <h3 className='font-semibold'>{grupo.grupo}</h3>
                 <span className='text-xs text-center px-1 py-[1px] bg-red-100 rounded-xl'>{formatCurrency(totalDespesas)}</span>
             </div>
-            <div className='flex flex-col gap-2 py-3 justify-between border-b border-slate-500'>
+            <div className='flex flex-col gap-2 py-3 justify-between'>
                 {grupo.despesas.map((despesa) => (
-                    <div key={despesa.nome} className='flex border-b border-slate-200  justify-between'>
-                        <div className='flex gap-1  items-start'>
+                    <div key={despesa.nome} className='flex justify-between'>
+                        <div className='flex gap-1 items-start'>
                             <p>{despesa.nome}</p>
                             <p
                                 style={{
